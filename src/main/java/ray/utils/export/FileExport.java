2f324dd8c88fc8b00ddec081c9f23586084d6244
poWERmaXray <speedcrusade0983@163.com>
2022-12-07 20:17:32 +0800

    [code][+] finish json export

--- src/main/java/ray/utils/export/FileExport.java
+++ src/main/java/ray/utils/export/FileExport.java
@@ -1,4 +1,7 @@
 package ray.utils.export;
 
+import java.io.File;
+
 public interface FileExport {
+    File exportJSONFile(File file, File outputFile, Object genericObject);
 }
--- src/main/java/ray/utils/export/impl/JsonFileExport.java
+++ src/main/java/ray/utils/export/impl/JsonFileExport.java
@@ -1,4 +1,29 @@
 package ray.utils.export.impl;
 
-public class JsonFileExport {
+import com.alibaba.fastjson2.JSON;
+import org.springframework.beans.factory.annotation.Autowired;
+import ray.utils.export.FileExport;
+import ray.utils.reader.FileReader;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+
+public class JsonFileExport implements FileExport {
+    @Autowired
+    private FileReader reader;
+
+    @Override
+    public File exportJSONFile(File file, File outputFile, Object genericObject) {
+        Object iniInstance = reader.getInstance(Path.of(file.getPath()), genericObject);
+
+        String exportString = JSON.toJSONString(iniInstance);
+        try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(outputFile), StandardCharsets.UTF_8)) {
+            writer.write(exportString);
+        } catch (Exception ignore) {
+        }
+        return outputFile;
+    }
 }
